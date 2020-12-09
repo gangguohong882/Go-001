697c3adc1415c2d6a429f2f7a638421f6fd947c1
gangguohong882 <baozhukan@163.com>
2020-12-09 23:13:40 +0800

    week03

---
+++ Week03/week03.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type httpHandler struct {
+}
+
+func (h *httpHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World\n")
+}
+
+type HttpServer struct {
+	http *http.Server
+}
+
+func NewHttpServer() *HttpServer {
+	mux := http.NewServeMux()
+	mux.Handle("/", &httpHandler{})
+
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	return &HttpServer{
+		http: httpServer,
+	}
+}
+
+func (h *HttpServer) StartSvc() error {
+		log.Println("start http server")
+	if err := h.http.ListenAndServe(); err != http.ErrServerClosed {
+		return err
+	}
+
+	log.Println("exit http server")
+	return nil
+}
+
+func (h *HttpServer) StopSvc(ctx context.Context) error {
+	if err := h.http.Shutdown(ctx); err != nil {
+		log.Fatal("stop http server fail")
+		return err
+	}
+	log.Println("stop http server success")
+	return nil
+}
+
+func main() {
+	eg, ctx := errgroup.WithContext(context.Background())
+
+	httpServer := NewHttpServer()
+
+	eg.Go(func() error {
+		return httpServer.StartSvc()
+	})
+
+	eg.Go(func() error {
+		sign := make(chan os.Signal, 1)
+		signal.Notify(sign, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case sig := <-sign:
+			log.Printf("receive close: %s", sig.String())
+
+		case <-ctx.Done():
+			log.Println("http start fail")
+		}
+
+		return httpServer.StopSvc(ctx)
+
+	})
+
+	if err := eg.Wait(); err != nil {
+		log.Printf("server exit error: %+v", err)
+		return
+	}
+
+	log.Println("server exit")
+}
